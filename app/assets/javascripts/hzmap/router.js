0622b735b72e9f8f6386ea1867aa4d89f1059d40
Jonathan <jon.dandois@gmail.com>
2017-06-14 17:06:44 -0400

    push router back into search bar

--- app/assets/javascripts/hzmap/router.js
+++ app/assets/javascripts/hzmap/router.js
@@ -11,7 +11,30 @@ HZApp.Router = (function(){
       HZApp.Router.router.resolve();
     },
     handleRouter: function(params, query){
-      console.log('Router: ', params, query);
+      console.log('Router: ');
+      var latlng_q = HZApp.Router.parseQueryByName(query, 'latlng');
+      var search_q = HZApp.Router.parseQueryByName(query, 'search');
+      if (latlng_q){
+        console.log('latlng query: ', latlng_q);
+      } else if (search_q) {
+        var input_field = document.getElementById('search-field-small');
+        input_field.value = search_q.search;
+      } else {
+        console.log('no valid search parameters detected!');
+      }
+    },
+    // return a new object for a single query parameter
+    parseQueryByName: function(query, name){
+      var query_split = query.split('?');
+      for (var i = query_split.length - 1; i >= 0; i--) {
+        var sub_split = query_split[i].split('=');
+        if (sub_split[0] === name){
+          var query_obj = {};
+          query_obj[sub_split[0]] = decodeURI(sub_split[1]);
+          return query_obj;
+        }
+      }
     },
   }
 })();
+
