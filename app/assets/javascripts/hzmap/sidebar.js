d2aaa4f2bed1c10bc5346d202fcb9545710c2a31
Jonathan <jon.dandois@gmail.com>
2017-06-29 15:44:57 -0400

    HUB-604 use regular for loop, instead of extra crispy

--- app/assets/javascripts/hzmap/sidebar.js
+++ app/assets/javascripts/hzmap/sidebar.js
@@ -1,16 +1,6 @@
 //Sidebar utils
 HZApp.SidebarUtils = (function(){
 
-  // forEach polyfill for IE
-  if (window.NodeList && !NodeList.prototype.forEach) {
-    NodeList.prototype.forEach = function (callback, argument) {
-      argument = argument || window;
-      for (var i = 0; i < this.length; i++) {
-          callback.call(argument, this[i], i, this);
-      }
-    };
-  }
-
   // extend jquery with our sidebar function
   $.fn.sidebar = function() {
     var $sidebar = this;
@@ -107,9 +97,9 @@ HZApp.SidebarUtils = (function(){
       // update the accordion statuse
       var detailsRows = document.querySelectorAll('.designation-details-row');
 
-      detailsRows.forEach(function(detail){
-        detail.hidden = !visibleState;
-      });
+      for (var i = detailsRows.length - 1; i >= 0; i--) {
+        detailsRows[i].hidden = !visibleState;
+      }
 
       // update the button link
       document.querySelector('span.additional-details-expand.show').hidden = (action === 'show' ? true : false);
